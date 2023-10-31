0a585037b34dab08dda1813ebefeb8cf8ce94157
mavargas <97851695+computerSmokio@users.noreply.github.com>
2023-10-30 23:09:08 -0300

    Delete build directory
    forget the gitignore

--- build/main.py
+++
@@ -1,9 +0,0 @@
-from api.config import Config
-from api.logger import gLogger
-from api.main_api import create as create_api
-from wsgiref.simple_server import make_server
-
-if __name__ == '__main__':
-    with make_server('', Config.PORT, create_api()) as server:
-        gLogger.info('Server started')
-        server.serve_forever() 
